68d47ec9d06d34b794f5e97e7ba2ee6c33f35949
ravishhmb <76579907+ravishhmb@users.noreply.github.com>
2021-01-19 16:42:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,18 +22,3 @@ let letter = '';
 
 }());
 
-
-$('.skill-per').each(function(){
-    var $this = $(this);
-    var per = $this.attr('per');
-    $this.css("width",per + '%');
-    $({animatedValue: 0}).animate({animatedValue: per},{
-        duration:1000,
-        stop:function(){
-            $this.attr('per',Math.floor(this.animatedValue) + '%');
-        },
-        complete:function(){
-            $this.attr('per',Math.floor(this.animatedValue) + '%');
-        }
-    });
-});
